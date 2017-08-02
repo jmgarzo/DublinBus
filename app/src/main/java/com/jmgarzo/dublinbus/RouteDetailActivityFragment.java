870978fb2331365d0625fb58a276c0e0f7eabc49
jmgarzo <jmgarzo@localhost.localdomain>
2017-08-02 12:01:13 +0100

    RouteActivityFragment recive Route object

--- app/src/main/java/com/jmgarzo/dublinbus/RouteDetailActivityFragment.java
+++ app/src/main/java/com/jmgarzo/dublinbus/RouteDetailActivityFragment.java
@@ -1,15 +1,20 @@
 package com.jmgarzo.dublinbus;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jmgarzo.dublinbus.objects.Route;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class RouteDetailActivityFragment extends Fragment {
+
+    Route route;
 
     public RouteDetailActivityFragment() {
     }
@@ -17,6 +22,13 @@ public class RouteDetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_route_detail, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_route_detail, container, false);
+        Intent intent = getActivity().getIntent();
+        if (null != intent) {
+            route = intent.getParcelableExtra(RouteActivity.ROUTE_INTENT_TAG);
+        }
+
+        return rootView;
     }
 }
