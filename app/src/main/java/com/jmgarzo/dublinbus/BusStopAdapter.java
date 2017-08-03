98c1dea139f972d5f5b9f58572121a73a562e4b7
Jose Maria Garzo <jmgarzo@gmail.com>
2017-08-03 19:15:16 +0100

    -Fix error in RouteBusStopTable we was inserting bus_stop_number, but we need bus_stop_id -New field in route_bus_stop table to save the order of the bus_stops and show bus stops in order 	new file:   app/src/main/java/com/jmgarzo/dublinbus/BusStopAdapter.java 	new file:   app/src/main/res/layout/list_item_bus_stop.xml

---
+++ app/src/main/java/com/jmgarzo/dublinbus/BusStopAdapter.java
@@ -0,0 +1,95 @@
+package com.jmgarzo.dublinbus.utilities;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jmgarzo.dublinbus.R;
+import com.jmgarzo.dublinbus.objects.Route;
+
+/**
+ * Created by jmgarzo on 03/08/17.
+ */
+
+public class BusStopAdapter extends RecyclerView.Adapter<BusStopAdapter.BusStopAdapterViewHolder> {
+
+    private Cursor mCursor;
+
+    private Context mContext;
+    private final BusStopAdapterOnClickHandler mClickHandler;
+
+
+    public static interface BusStopAdapterOnClickHandler {
+        void onClick(Route route);
+    }
+
+    public BusStopAdapter(Context context, BusStopAdapterOnClickHandler clickHandler){
+        mContext = context;
+        mClickHandler = clickHandler;
+    }
+
+    @Override
+    public BusStopAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+
+        View view = inflater.inflate(R.layout.list_item_bus_stop, parent, false);
+        view.setFocusable(true);
+        return new BusStopAdapterViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(BusStopAdapterViewHolder holder, int position) {
+
+        mCursor.moveToPosition(position);
+
+        if (mCursor != null && mCursor.moveToPosition(position)) {
+            holder.mBusStopName.setText(mCursor.getString(DBUtils.COL_BUS_STOP_NUMBER));
+            holder.mBusStopShortName.setText(mCursor.getString(DBUtils.COL_BUS_STOP_SHORTNAME));
+            holder.mBusStopShortNameLocalized.setText(mCursor.getString(DBUtils.COL_BUS_STOP_SHORT_NAME_LOCALIZED));
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (null == mCursor) return 0;
+        return mCursor.getCount();
+    }
+
+    void swapCursor(Cursor newCursor) {
+        mCursor = newCursor;
+        notifyDataSetChanged();
+    }
+
+
+
+    public class BusStopAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final TextView mBusStopName;
+        public final TextView mBusStopShortName;
+        public final TextView mBusStopShortNameLocalized;
+
+        public BusStopAdapterViewHolder(View view) {
+            super(view);
+            mBusStopName = (TextView) view.findViewById(R.id.list_item_bus_stop_name);
+            mBusStopShortName = (TextView) view.findViewById(R.id.list_item_bus_stop_short_name);
+            mBusStopShortNameLocalized = (TextView) view.findViewById(R.id.list_item_bus_stop_short_name_localized);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            Route route = new Route(mCursor, adapterPosition);
+            mClickHandler.onClick(route);
+
+
+        }
+
+
+    }
+}
