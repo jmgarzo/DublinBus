84cd46ae159df3228d38fcf1c44c7ddec0122ba8
jmgarzo <jmgarzo.developer@gmail.com>
2017-08-31 11:31:50 +0100

    Changed admod for devices

--- app/src/main/java/com/jmgarzo/dublinbus/utilities/AdUtils.java
+++ app/src/main/java/com/jmgarzo/dublinbus/utilities/AdUtils.java
@@ -11,8 +11,8 @@ public class AdUtils {
     public static AdRequest getAdRequest() {
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .addTestDevice("7A96701D1162F3BAA9884E567FDE869A")
-                .addTestDevice("1C1F83B043C4EB13E7A72D25906BBFA4")
+//                .addTestDevice("7A96701D1162F3BAA9884E567FDE869A")
+//                .addTestDevice("1C1F83B043C4EB13E7A72D25906BBFA4")
                 .build();
         return  adRequest;
     }
